06cb7988b3d09a87af549440043e20320c6c92cd
journeyAa <68971806+journeyAa@users.noreply.github.com>
2020-08-10 01:47:58 -0400

    Update GitPractice.java

--- src/Git/GitPractice.java
+++ src/Git/GitPractice.java
@@ -4,6 +4,8 @@ public class GitPractice {
     public static void main(String[] args) {
 
         // this is for practice agan
+        // this line is added on the gitHub page. 
+        //Hopefully I will see this on intelliJ
 
     }
 }
