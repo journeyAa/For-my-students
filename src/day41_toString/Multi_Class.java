d52e5db6b101c002057abe48a728011659791f38
journeyAa <Aalia.Alper515@gmail.com>
2020-08-15 16:13:12 -0400

    Multi_Class.java

---
+++ src/day41_toString/Multi_Class.java
@@ -0,0 +1,32 @@
+package day41_toString;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Multi_Class {
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+}
+class Test1{
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+}
+class Test2{
+    public static void main(String[] args) {
+        System.out.println("wow");
+
+    }
+
+}
