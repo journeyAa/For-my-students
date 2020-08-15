9e0d45b9bdf8e452ff1790f5de3fb153edfdd118
journeyAa <Aalia.Alper515@gmail.com>
2020-08-15 11:48:53 -0400

    CarpetObjects.java

--- src/day41_Static/Carpet.java
+++ src/day41_Static/Carpet.java
@@ -18,6 +18,27 @@ public class Carpet {
 
     }
 
+    public double calcCost(){
+        double totalPrice = (width*length)*unitPrice;
+
+        if(isPersian){
+            totalPrice+=200;
+        }
+
+        // return isPersian? totalPrice +200: totalPrice;
+        return totalPrice;
+
+    }
+    public void getCarpetInfo(){
+        System.out.println("======================================================================");
+        System.out.println("Width: " + width);
+        System.out.println("Length: " + length);
+        System.out.println("Unit Price: " + unitPrice);
+        System.out.println("Persian Carpet: " + isPersian);
+        System.out.println("Total Price: " + calcCost());
+        System.out.println("=======================================================================");
+    }
+
 
 
 
