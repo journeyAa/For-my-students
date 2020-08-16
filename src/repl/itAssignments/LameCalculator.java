7fb1d5d7a61825d2d4769547d29ff98e3c6cad3d
journeyAa <Aalia.Alper515@gmail.com>
2020-08-15 23:31:51 -0400

    LameCalculator.java

---
+++ src/repl/itAssignments/LameCalculator.java
@@ -0,0 +1,41 @@
+package repl.itAssignments;
+
+public class LameCalculator {
+
+    int num1;
+    char method;
+    int num2;
+
+    public void Calculator(int num1, char method, int num2){
+        this.num1 = num1;
+        this.method = method;
+        this.num2 = num2;
+
+    }
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+    public int minus(int num1, int num2){
+        return num1-num2;
+    }
+    public int multiply(int num1, int num2){
+        return num1*num2;
+    }
+    public int divide(int num1, int num2){
+        return num1/num2;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
